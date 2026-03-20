20dcddd4cfa5c461f63d3383b52430ebef64d293
enginabdan <engin.abdan@gmail.com>
2026-03-20 15:06:21 -0400

    Switch production API base to api.bosembo.net

--- frontend/runtime-config.js
+++ frontend/runtime-config.js
@@ -5,6 +5,6 @@
   window.__APP_CONFIG__ = {
     API_BASE: isLocal
       ? "http://127.0.0.1:8000"
-      : "https://financegame-api-919842388268.us-central1.run.app",
+      : "https://api.bosembo.net",
   };
 })();
