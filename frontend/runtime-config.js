d4bec2b8d28948ae086e2eef018526dbd9ade8c5
enginabdan <engin.abdan@gmail.com>
2026-03-22 18:15:05 -0400

    dfs

--- frontend/runtime-config.js
+++ frontend/runtime-config.js
@@ -5,7 +5,7 @@
   window.__APP_CONFIG__ = {
     API_BASE: isLocal
       ? "http://127.0.0.1:8000"
-      : "https://api.bosembo.net",
+      : "https://financegame-api-919842388268.us-central1.run.app",
     FIREBASE_WEB_API_KEY: "AIzaSyDjsUtglMdBGcsOz4EDUUd6_LTQ9jMzj_c",
     TEACHER_KEY: "tmsacharlottesecondaryschoolteacherkey",
     STUDENT_SIGNUP_KEY: "tmsacharlottesecondaryschoolstudentkey",
